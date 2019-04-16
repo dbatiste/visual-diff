eb3fc7b9f83d037824608d90e1b4dc7f6e230a10
Dave Batiste <dave.batiste@d2l.com>
2019-04-16 19:20:38 -0400

    Add reset focus helper.

--- visual-diff.js
+++ visual-diff.js
@@ -101,6 +101,19 @@ class VisualDiff {
 		else await this._compare(name);
 	}
 
+	async resetFocus(page) {
+		await page.evaluate(() => {
+			let elem = document.querySelector('#vd-focus');
+			if (!elem) {
+				elem = document.createElement('button');
+				elem.id = 'vd-focus';
+				elem.innerHTML = 'reset focus';
+				document.body.insertBefore(elem, document.body.firstChild);
+			}
+		});
+		await page.click('#vd-focus');
+	}
+
 	async _compare(name) {
 
 		const currentImage = await this._fs.getCurrentImage(name);
